1d2f2cc91c77e148a07e4a816b84647821395c74
s3a317097 <gm10285@gm.student.ncut.edu.tw>
2017-12-13 11:43:07 +0800

    shop_revenue_details_1

--- resources/views/shop_revenue_details.blade.php
+++ resources/views/shop_revenue_details.blade.php
@@ -5,21 +5,32 @@
 <section class="row text-center placeholders">
     <div class="col-6 col-sm-6 placeholder">
         <canvas id="myChart" width="700" height="400"></canvas>
-        <h4>Label</h4>
-        <span class="text-muted">Something else</span>
+        <h4>各月營收</h4>
     </div>
     <div class="col-6 col-sm-6 placeholder">
         <canvas id="myChart2" width="700" height="400"></canvas>
-        <h4>Label</h4>
-        <span class="text-muted">Something else</span>
+        <h4>出貨情況</h4>
+        
     </div>
 </section>
-<h2>營收明細</h2>
-<div class="table-responsive">
+<div class='col-12'>
+    <div class='row'>
+        <h2 class='mr-5'>營收明細</h2>
+        <form class="form-inline">
+            <div class="form-group">
+                <label class="col-sm-12 col-form-label">搜尋id:</label>
+            </div>
+            <div class="form-group mx-sm-12">
+                <input type="text" class="form-control" id="table_id" placeholder="搜尋id">
+            </div>
+        </form>
+     
+    </div>
+<div class="table-responsive mt-3">
             <table class="table table-striped">
               <thead>
                 <tr>
-                  <th>#</th>
+                  <th>id</th>
                   <th>Header</th>
                   <th>Header</th>
                   <th>Header</th>
@@ -28,112 +39,21 @@
               </thead>
               <tbody>
                 <tr>
-                  <td>1,001</td>
-                  <td>Lorem</td>
-                  <td>ipsum</td>
-                  <td>dolor</td>
-                  <td>sit</td>
-                </tr>
-                <tr>
-                  <td>1,002</td>
-                  <td>amet</td>
-                  <td>consectetur</td>
-                  <td>adipiscing</td>
-                  <td>elit</td>
-                </tr>
-                <tr>
-                  <td>1,003</td>
-                  <td>Integer</td>
-                  <td>nec</td>
-                  <td>odio</td>
-                  <td>Praesent</td>
-                </tr>
-                <tr>
-                  <td>1,003</td>
-                  <td>libero</td>
-                  <td>Sed</td>
-                  <td>cursus</td>
-                  <td>ante</td>
-                </tr>
-                <tr>
-                  <td>1,004</td>
-                  <td>dapibus</td>
-                  <td>diam</td>
-                  <td>Sed</td>
-                  <td>nisi</td>
-                </tr>
-                <tr>
-                  <td>1,005</td>
-                  <td>Nulla</td>
-                  <td>quis</td>
-                  <td>sem</td>
-                  <td>at</td>
-                </tr>
-                <tr>
-                  <td>1,006</td>
-                  <td>nibh</td>
-                  <td>elementum</td>
-                  <td>imperdiet</td>
-                  <td>Duis</td>
-                </tr>
-                <tr>
-                  <td>1,007</td>
-                  <td>sagittis</td>
-                  <td>ipsum</td>
-                  <td>Praesent</td>
-                  <td>mauris</td>
-                </tr>
-                <tr>
-                  <td>1,008</td>
-                  <td>Fusce</td>
-                  <td>nec</td>
-                  <td>tellus</td>
-                  <td>sed</td>
-                </tr>
-                <tr>
-                  <td>1,009</td>
-                  <td>augue</td>
-                  <td>semper</td>
-                  <td>porta</td>
-                  <td>Mauris</td>
-                </tr>
-                <tr>
-                  <td>1,010</td>
-                  <td>massa</td>
-                  <td>Vestibulum</td>
-                  <td>lacinia</td>
-                  <td>arcu</td>
-                </tr>
-                <tr>
-                  <td>1,011</td>
-                  <td>eget</td>
-                  <td>nulla</td>
-                  <td>Class</td>
-                  <td>aptent</td>
-                </tr>
-                <tr>
-                  <td>1,012</td>
-                  <td>taciti</td>
-                  <td>sociosqu</td>
-                  <td>ad</td>
-                  <td>litora</td>
-                </tr>
-                <tr>
-                  <td>1,013</td>
+                  <td>1</td>
                   <td>torquent</td>
                   <td>per</td>
                   <td>conubia</td>
                   <td>nostra</td>
                 </tr>
                 <tr>
-                  <td>1,014</td>
+                  <td>2</td>
                   <td>per</td>
                   <td>inceptos</td>
                   <td>himenaeos</td>
                   <td>Curabitur</td>
                 </tr>
                 <tr>
-                  <td>1,015</td>
+                  <td>3</td>
                   <td>sodales</td>
                   <td>ligula</td>
                   <td>in</td>
@@ -141,18 +61,31 @@
                 </tr>
               </tbody>
             </table>
-          </div>
+</div>
+</div>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.0/Chart.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.0/Chart.bundle.js"></script>
 <script>
+$(document).ready(function(){ 
+    $.ajax({
+        url: '/rest/api/buy/',
+        dataType: "json",
+        type: 'get',
+        data: {orderlist:id,shop_id:shop},
+        success: function (data) {
+            
+        }
+    });
+    
+});
 var ctx = document.getElementById("myChart").getContext('2d');
 var myChart = new Chart(ctx, {
-    type: 'bar',
+    type: 'line',
     data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        labels: ["1月", "2月", "3月", "4月", "5月", "6月","7月", "8月", "9月", "10月", "11月", "12月"],
         datasets: [{
             label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            data: [12, 19, 3, 5, 2, 3,12, 19, 3, 5, 2, 3],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
@@ -218,6 +151,7 @@ var myChart = new Chart(ctx, {
             }]
         }
     }
+
 });
 </script>
 @endsection
